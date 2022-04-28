dee65dd3ce5580b0664f966ba4bc3c8e9b5316a7
woo-song-Lee <twocowsong@uniwiz.co.kr>
2022-04-29 08:21:37 +0900

    공통코드화면 연결 반영건

--- src/main/java/com/twocow/song/enums/Menu.java
+++ src/main/java/com/twocow/song/enums/Menu.java
@@ -6,11 +6,12 @@ import lombok.Getter;
 public enum Menu {
 	NONE("NONE" , "미설정" , Role.R000, false),
 	SAMPLE("SAMPLE" , "샘플" , Role.R000, false),
-	MAIN("MAIN", "메인", Role.R000, true),
 	COMMON_MAIN("COMMON_MAIN", "메인", Role.R000, false),
 	MY_INFO("MY_INFO", "MyInfo", Role.R000, true),
-	BOARD_MANAGEMENT("BOARD_MANAGEMENT", "BoardManagement", Role.R000, true),
 
+	MAIN("MAIN", "메인", Role.R001, true),
+	BOARD_MANAGEMENT("BOARD_MANAGEMENT", "BoardManagement", Role.R001, true),
+	COMMON_MANAGEMENT("COMMON_MANAGEMENT", "CommonManagement", Role.R002, true),
 
 	USER_LOGIN("USER_LOGIN", "로그인" , Role.R000, false),
 	USER_LOGOUT("USER_LOGOUT", "로그아웃" , Role.R000, false),
--- src/main/java/com/twocow/song/mvc/admin/controller/main/AdminMainController.java
+++ src/main/java/com/twocow/song/mvc/admin/controller/main/AdminMainController.java
@@ -2,15 +2,19 @@ package com.twocow.song.mvc.admin.controller.main;
 
 import com.twocow.song.configuration.annotation.RequestConfig;
 import com.twocow.song.enums.Menu;
+import com.twocow.song.enums.common.CommonCode;
 import com.twocow.song.mvc.admin.service.board.BoardService;
 import com.twocow.song.mvc.admin.vo.board.Board;
 import com.twocow.song.mvc.admin.vo.board.BoardPageInfo;
+import com.twocow.song.mvc.common.mvc.service.CommonService;
+import com.twocow.song.mvc.common.vo.Code;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 @Controller
@@ -19,6 +23,7 @@ import java.util.LinkedList;
 public class AdminMainController {
 
 	private final BoardService boardService;
+	private final CommonService commonService;
 
 	@GetMapping("/main")
 	@RequestConfig(menu = Menu.MAIN, login = true)
@@ -28,10 +33,18 @@ public class AdminMainController {
 
 	@GetMapping("/board/management")
 	@RequestConfig(menu = Menu.BOARD_MANAGEMENT, login = true)
-	public String management(Model model, BoardPageInfo boardPageInfo) {
+	public String boardManagement(Model model, BoardPageInfo boardPageInfo) {
 		LinkedList<Board> boardInfo = boardService.getBoardInfo(boardPageInfo);
 		model.addAttribute("list", boardInfo);
 		return "/admin/board/management";
+	}
+
+	@GetMapping("/common/management")
+	@RequestConfig(menu = Menu.COMMON_MANAGEMENT, login = true)
+	public String commonManagement(Model model) {
+		ArrayList<Code> code = commonService.getCode(CommonCode.BOARD.name());
+		model.addAttribute("codeList", code);
+		return "/admin/common/management";
 	}
 
 }
